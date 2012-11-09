50cdb5ad70214c5525f51be44f7c6cd770c113f1
Nikolaos Papadopoulos <nikolaspapadop@yahoo.com>
2012-11-09 15:52:44 +0100

    sec struct ready

--- src/PSS/PssReader.java
+++ src/PSS/PssReader.java
@@ -1,7 +1,5 @@
 package PSS;
 
-import static resources.AminoAcids.*;
-
 import java.io.File;
 import java.io.IOException;
 import java.util.Collection;
@@ -19,7 +17,31 @@ public class PssReader {
 		this.ssccString = dir;
 	}
 
-	public PSSResult calcSecStructPref() throws IOException {
+	/**
+	 * A wrapper function, to make things look a bit better on the outside
+	 * 
+	 * @return the Secondary Structure Preference scores in form of a double
+	 *         array
+	 * @throws IOException
+	 */
+	public double[][] calcPSSScore() throws IOException {
+		PSSMeasurement m = measureFrequencies();
+		return calcSeqStrucPref(m);
+	}
+
+	/**
+	 * this function reads the sscc files and collects all data needed for the
+	 * calculation of the secondary structure preference score, as defined in
+	 * the paper. This measurements are saved as a PSSMeasurement object.
+	 * 
+	 * @return a {@link PSSMeasurement} object, containing all information
+	 *         needed for calculation of the SSP score. This information is
+	 *         N(i,s) [number of amino acids i in structure s], N(i) [number of
+	 *         amino acids i], N(s) [number of amino acids in structure s] and N
+	 *         [total number of amino acids]
+	 * @throws IOException
+	 */
+	private PSSMeasurement measureFrequencies() throws IOException {
 		File ssccDir = new File(ssccString);
 		Collection<File> ssccFiles = FileUtils.listFiles(ssccDir,
 				TrueFileFilter.INSTANCE, TrueFileFilter.INSTANCE);
@@ -48,7 +70,47 @@ public class PssReader {
 				}
 			}
 		}
-		return new PSSResult(prefArray, occurAmino, occurStruct, totalAmino);
+
+		return new PSSMeasurement(prefArray, occurAmino, occurStruct,
+				totalAmino);
+	}
+
+	/**
+	 * Performs the log odds calculation needed to provide the Secondary
+	 * Structure Preference scores for all amino acids.
+	 * 
+	 * @param m
+	 *            a {@link PSSMeasurement} object
+	 * @return the score table, containing the PSS score for every amino acid
+	 *         and every secondary structure (here alpha helix, beta strand and
+	 *         coil)
+	 */
+	private double[][] calcSeqStrucPref(PSSMeasurement m) {
+		double[][] secStrucPreference = new double[20][3];
+		double[][] expectedSecStruc = new double[20][3];
+		double n_i = 0;
+		double n_s = 0;
+		double up = 0;
+		double n_total = (m.getTotalAmino() * 1.0);
+		for (int i = 0; i < 20; i++) {
+			for (int j = 0; j < 3; j++) {
+				n_i = m.getOccurAmino()[i];
+				n_s = m.getOccurStruct()[j];
+				up = n_i * n_s;
+				expectedSecStruc[i][j] = up;
+			}
+		}
+
+		double odds = 0;
+		for (int i = 0; i < 20; i++) {
+			for (int j = 0; j < 3; j++) {
+				odds = m.getPrefArray()[i][j] * n_total
+						/ expectedSecStruc[i][j];
+				// System.out.println(odds);
+				secStrucPreference[i][j] = -Math.log(odds);
+			}
+		}
+		return secStrucPreference;
 	}
 
 }
--- src/pssTest.java
+++ src/pssTest.java
@@ -1,12 +1,25 @@
 import java.io.IOException;
 
+import resources.AminoAcids;
+
 import PSS.PssReader;
 
 public class pssTest {
 	public static void main(String[] args) throws IOException {
 		String dir = "sscc/";
 		PssReader reader = new PssReader(dir);
-		reader.calcSecStructPref();
+		double[][] result = reader.calcPSSScore();
+		for(int i = 0; i < 20; i++) {
+			System.out.print(AminoAcids.reverse[i] + "\t");
+		}
 		System.out.println();
+		for (int i = 0; i < 3; i++) {
+			for(int j = 0; j < 20; j++) {
+				System.out.print((int)(result[j][i]*100) + "\t");
+			}
+			System.out.println();
+		}
+//		double logodds = 1.414;
+//		System.out.println(Math.log(logodds));
 	}
 }
